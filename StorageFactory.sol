f80b3e718b5ce97eefb1c77e33349509493b1b2d
obynonwane <biostechnologyng@gmail.com>
2024-07-24 13:53:19 +0100

    storing deployed contracte into storage in an array

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18; // stating our solidity version
 
-import "./SimpleStorage.sol";
+import { SimpleStorage } from "./SimpleStorage.sol";
 
 contract StorageFactory {
 
 
-    SimpleStorage public  simpleStorage;
+    SimpleStorage[] public  listOfSimpleStorageContract;
     //create a function to deploy SimpleStorage contract
     // and then save it in a state variable 
     function createSimpleStorageContract () public {
-
         //the new keyword is how solidity know to deploy a contract
-        simpleStorage = new SimpleStorage();
+        // simpleStorage = new SimpleStorage();
+         listOfSimpleStorageContract.push(new SimpleStorage());
 
     }
 }
