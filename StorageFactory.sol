5551cd84e43411c720e301702c5f3020aa70a718
obynonwane <biostechnologyng@gmail.com>
2024-07-24 12:32:53 +0100

    added Storage factory that will deploy our simple storage contract

---
+++ StorageFactory.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18; // stating our solidity version
+
+import "./SimpleStorage.sol";
+
+contract StorageFactory {
+    //create a function to deploy SimpleStorage contract
+    // and then save it in a state variable 
+
+    function createSimpleStorageContract () public {
+
+    }
+}
