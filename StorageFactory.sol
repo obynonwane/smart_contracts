0f2df8a0f4df8582cb8d6a0b1bd20d5e43b49145
obynonwane <biostechnologyng@gmail.com>
2024-07-24 12:50:27 +0100

    added variable to hold deployed contract simple storage

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -4,10 +4,15 @@ pragma solidity ^0.8.18; // stating our solidity version
 import "./SimpleStorage.sol";
 
 contract StorageFactory {
+
+
+    SimpleStorage public  simpleStorage;
     //create a function to deploy SimpleStorage contract
     // and then save it in a state variable 
-
     function createSimpleStorageContract () public {
+
+        //the new keyword is how solidity know to deploy a contract
+        simpleStorage = new SimpleStorage();
 
     }
 }
